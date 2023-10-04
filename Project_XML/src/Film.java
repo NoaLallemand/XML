26541c9ab6a5f8457ba4e7abeab43fda00a66061
NoaLallemand <noa.lallemand@student.hepl.be>
2023-10-04 15:40:19 +0200

    Creation Classe Movie

---
+++ Project_XML/src/Film.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Movie {
+    public int id;
+    public String title;
+    public String originalTitle;
+    public Date releaseDate;
+    public String status;
+
+    public String convertToXml() {
+
+    }
+}
